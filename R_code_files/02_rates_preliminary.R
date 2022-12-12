4c91fd14b2b7f672e21885b09bc6111024f8398c
Daniel Bunn <dbunn@taxfoundation.org>
2022-12-12 09:07:46 -0500

    02_rates_preliminary.R Update

--- R_code_files/02_rates_preliminary.R
+++ R_code_files/02_rates_preliminary.R
@@ -18,77 +18,76 @@ oecd_data_2022 <- get_dataset("TABLE_II1", start_time = 2022, end_time = 2022)
 oecd_data_2022 <- subset(oecd_data_2022, oecd_data_2022$CORP_TAX=="COMB_CIT_RATE")
 oecd_data_2022 <- subset(oecd_data_2022, select = -c(CORP_TAX,TIME_FORMAT,Time))
 
-colnames(oecd_data_2022)[colnames(oecd_data_2022)=="obsValue"] <- "2022"
+colnames(oecd_data_2022)[colnames(oecd_data_2022)=="ObsValue"] <- "2022"
 colnames(oecd_data_2022)[colnames(oecd_data_2022)=="COU"] <- "iso_3"
 
-#KPMG Rows are no longer applicable now that KPMG dataset is no longer being updated
+
 #KPMG Data####
 
 #Read in dataset
-kpmg_data_2021 <- read_excel("source_data/kpmg_dataset_2010_2021.xlsx")
+#kpmg_data_2021 <- read_excel("source_data/kpmg_dataset_2010_2021.xlsx")
 
 #Keep and rename selected columns
-kpmg_data_2021 <- kpmg_data_2021[,-c(2:11)]
-colnames(kpmg_data_2021)[colnames(kpmg_data_2021)=="LOCATION"] <- "country"
+#kpmg_data_2021 <- kpmg_data_2021[,-c(2:11)]
+#colnames(kpmg_data_2021)[colnames(kpmg_data_2021)=="LOCATION"] <- "country"
 
 #Change KPMG country names to match official ISO-names
 
-kpmg_data_2021$country[kpmg_data_2021$country == "Bolivia"] <- "Bolivia (Plurinational State of)"
+#kpmg_data_2021$country[kpmg_data_2021$country == "Bolivia"] <- "Bolivia (Plurinational State of)"
 
-kpmg_data_2021$country[kpmg_data_2021$country == "Bonaire, Saint Eustatius and Saba"] <- "Bonaire, Sint Eustatius and Saba"
+#kpmg_data_2021$country[kpmg_data_2021$country == "Bonaire, Saint Eustatius and Saba"] <- "Bonaire, Sint Eustatius and Saba"
 
-kpmg_data_2021$country[kpmg_data_2021$country == "Ivory Coast"] <- "Cote d'Ivoire"
+#kpmg_data_2021$country[kpmg_data_2021$country == "Ivory Coast"] <- "Cote d'Ivoire"
 
-kpmg_data_2021$country[kpmg_data_2021$country == "Hong Kong SAR"] <- "China, Hong Kong Special Administrative Region"
+#kpmg_data_2021$country[kpmg_data_2021$country == "Hong Kong SAR"] <- "China, Hong Kong Special Administrative Region"
 
-kpmg_data_2021$country[kpmg_data_2021$country == "Macau"] <- "China, Macao Special Administrative Region"
+#kpmg_data_2021$country[kpmg_data_2021$country == "Macau"] <- "China, Macao Special Administrative Region"
 
-kpmg_data_2021$country[kpmg_data_2021$country == "Congo (Democratic Republic of the)"] <- "Democratic Republic of the Congo"
+#kpmg_data_2021$country[kpmg_data_2021$country == "Congo (Democratic Republic of the)"] <- "Democratic Republic of the Congo"
 
-kpmg_data_2021$country[kpmg_data_2021$country == "Czech Republic"] <- "Czechia"
+#kpmg_data_2021$country[kpmg_data_2021$country == "Czech Republic"] <- "Czechia"
 
-kpmg_data_2021$country[kpmg_data_2021$country == "Korea, Republic of"] <- "Republic of Korea"
+#kpmg_data_2021$country[kpmg_data_2021$country == "Korea, Republic of"] <- "Republic of Korea"
 
-kpmg_data_2021$country[kpmg_data_2021$country == "Macedonia"] <- "The former Yugoslav Republic of Macedonia"
+#kpmg_data_2021$country[kpmg_data_2021$country == "Macedonia"] <- "The former Yugoslav Republic of Macedonia"
 
-kpmg_data_2021$country[kpmg_data_2021$country == "Moldova"] <- "Republic of Moldova"
+#kpmg_data_2021$country[kpmg_data_2021$country == "Moldova"] <- "Republic of Moldova"
 
-kpmg_data_2021$country[kpmg_data_2021$country == "Palestinian Territory"] <- "State of Palestine"
+#kpmg_data_2021$country[kpmg_data_2021$country == "Palestinian Territory"] <- "State of Palestine"
 
-kpmg_data_2021$country[kpmg_data_2021$country == "Russia"] <- "Russian Federation"
+#kpmg_data_2021$country[kpmg_data_2021$country == "Russia"] <- "Russian Federation"
 
-kpmg_data_2021$country[kpmg_data_2021$country == "St Maarten"] <- "Saint Martin (French Part)"
+#kpmg_data_2021$country[kpmg_data_2021$country == "St Maarten"] <- "Saint Martin (French Part)"
 
-kpmg_data_2021$country[kpmg_data_2021$country == "Syria"] <- "Syrian Arab Republic"
+#kpmg_data_2021$country[kpmg_data_2021$country == "Syria"] <- "Syrian Arab Republic"
 
-kpmg_data_2021$country[kpmg_data_2021$country == "Tanzania"] <- "United Republic of Tanzania"
+#kpmg_data_2021$country[kpmg_data_2021$country == "Tanzania"] <- "United Republic of Tanzania"
 
-kpmg_data_2021$country[kpmg_data_2021$country == "United Kingdom"] <- "United Kingdom of Great Britain and Northern Ireland"
+#kpmg_data_2021$country[kpmg_data_2021$country == "United Kingdom"] <- "United Kingdom of Great Britain and Northern Ireland"
 
-kpmg_data_2021$country[kpmg_data_2021$country == "United States"] <- "United States of America"
+#kpmg_data_2021$country[kpmg_data_2021$country == "United States"] <- "United States of America"
 
-kpmg_data_2021$country[kpmg_data_2021$country == "Venezuela"] <- "Venezuela (Bolivarian Republic of)"
+#kpmg_data_2021$country[kpmg_data_2021$country == "Venezuela"] <- "Venezuela (Bolivarian Republic of)"
 
-kpmg_data_2021$country[kpmg_data_2021$country == "Vietnam"] <- "Viet Nam"
+#kpmg_data_2021$country[kpmg_data_2021$country == "Vietnam"] <- "Viet Nam"
 
 
 #Add ISO-Code to KPMG Data
-kpmg_data_iso <- merge(kpmg_data_2021, country_iso_cont, by="country", all=T)
+#kpmg_data_iso <- merge(kpmg_data_2021, country_iso_cont, by="country", all=T)
 
 #Remove continent averages
-kpmg_data_iso <- kpmg_data_iso[!grepl("average", kpmg_data_iso$country),]
+#kpmg_data_iso <- kpmg_data_iso[!grepl("average", kpmg_data_iso$country),]
 
- 
+
 #Merge OECD and KPMG Data####
 
-oecd_kpmg_2021 <- merge(oecd_data_2022, kpmg_data_iso, by="iso_3", all=T)
-oecd_kpmg_2021$`2021.x` <- if_else(is.na(oecd_kpmg_2021$`2021.x`), oecd_kpmg_2021$`2021.y`, oecd_kpmg_2021$`2021.x`)
-oecd_kpmg_2021 <- subset(oecd_kpmg_2021, select = -c(`2021.y`,iso_2))
+#oecd_kpmg_2021 <- merge(oecd_data_2021, kpmg_data_iso, by="iso_3", all=T)
+#oecd_kpmg_2021$`2021.x` <- if_else(is.na(oecd_kpmg_2021$`2021.x`), oecd_kpmg_2021$`2021.y`, oecd_kpmg_2021$`2021.x`)
+#oecd_kpmg_2021 <- subset(oecd_kpmg_2021, select = -c(`2021.y`,iso_2))
 
-colnames(oecd_kpmg_2021)[colnames(oecd_kpmg_2021)=="2021.x"] <- "2021"
+#colnames(oecd_kpmg_2021)[colnames(oecd_kpmg_2021)=="2021.x"] <- "2021"
 
-oecd_kpmg_2021 <- oecd_kpmg_2021[, c("2021", "iso_3", "country", "continent")]
-
+#oecd_kpmg_2021 <- oecd_kpmg_2021[, c("2021", "iso_3", "country", "continent")]
 
 #Dataset for previous years####
 
@@ -97,6 +96,7 @@ previous_years <- read_csv("source_data/data_rates_1980_2021.csv")
 
 #Drop column that is not needed
 previous_years <- subset(previous_years, select = -c(...1))
+
 
 #Read in OECD dataset for non-OECD Countries
 
@@ -127,20 +127,40 @@ colnames(previous_years_long)[colnames(previous_years_long)=="variable"] <- "yea
 previous_years_long$value <- as.numeric(previous_years_long$value)
 
 previous_years_long <- merge(previous_years_long, non_oecd_data, by=c("iso_3", "year"), all=T)
-####Left off here
-previous_years_long$value <- if_else(is.na(previous_years_long$value), previous_years_long$rate, previous_years_long$value)
+
+previous_years_long$value <- ifelse(is.na(previous_years_long$value), previous_years_long$rate, previous_years_long$value)
+
+
 previous_years_long <- subset(previous_years_long, select = -c(rate))
 colnames(previous_years_long)[colnames(previous_years_long)=="value"] <- "rate"
 
 previous_years <- spread(previous_years_long, year, rate)
 
 
-#Combine 2021 data ("oecd_kpmg_2021") with data from previous years ("previous_years")
+#Read in OECD dataset for non-OECD Countries for the year 2022
 
-oecd_kpmg_2021 <- subset(oecd_kpmg_2021, select = -c(country, continent))
+non_oecd_data_2022 <- get_dataset("CTS_CIT", start_time = 2022, end_time = 2022)
 
-all_years_preliminary <- merge(previous_years, oecd_kpmg_2021, by="iso_3", all=T)
+#Keep and rename selected columns
 
-all_years_preliminary <- all_years_preliminary[, c("iso_2", "iso_3", "continent", "country", 1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021)]
+non_oecd_data_2022 <- subset(non_oecd_data_2022, non_oecd_data_2022$CORP_TAX=="COMB_CIT_RATE")
+non_oecd_data_2022 <- subset(non_oecd_data_2022, select = -c(CORP_TAX,TIME_FORMAT,Time))
+
+colnames(non_oecd_data_2022)[colnames(non_oecd_data_2022)=="ObsValue"] <- "2022"
+colnames(non_oecd_data_2022)[colnames(non_oecd_data_2022)=="COU"] <- "iso_3"
+
+#Combine 2022 data ("oecd_2022") with 2022 non_OECD data
+oecd_all_2022 <- merge(oecd_data_2022, non_oecd_data_2022, by="iso_3", all=T)
+
+#Keep and rename selected columns
+colnames(oecd_all_2022)[colnames(oecd_all_2022)=="2022.x"] <- "redundant"
+colnames(oecd_all_2022)[colnames(oecd_all_2022)=="2022.y"] <- "2022"
+
+oecd_all_2022 <- subset(oecd_all_2022, select = -c(redundant))
+
+
+#Combine 2022 data ("oecd_all_2022") with data from previous years ("previous_years")
+all_years_preliminary <- merge(oecd_all_2022, previous_years, by="iso_3", all=T)
+all_years_preliminary <- all_years_preliminary[, c("iso_2", "iso_3", "continent", "country", 1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022)]
 
 write.csv(all_years_preliminary,"intermediate_outputs/rates_preliminary.csv")
