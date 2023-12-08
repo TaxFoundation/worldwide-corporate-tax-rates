0d6895de88a07f8bc272d5bea6fbb7d1cef13737
CristinaEnache <61510373+CristinaEnache@users.noreply.github.com>
2023-12-08 13:55:55 +0100

    Update Directory 00_master_file.R
    Update Directory

--- R_code_files/00_master_file.R
+++ R_code_files/00_master_file.R
@@ -6,11 +6,11 @@ gc()
 
 #Directory Variables####
 #setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-R_code_files<-"C:/Users/acer/Documents/Github/worldwide-corporate-tax-rates/R_code_files/"
-source_data<-"C:/Users/acer/Documents/Github/worldwide-corporate-tax-rates/source_data/"
-intermediate_outputs<-"C:/Users/acer/Documents/Github/worldwide-corporate-tax-rates/intermediate_outputs/"
-final_data<-"C:/Users/acer/Documents/Github/worldwide-corporate-tax-rates/final_data/"
-final_outputs<-"C:/Users/acer/Documents/Github/worldwide-corporate-tax-rates/final_outputs/"
+R_code_files<-"C:/Github/worldwide-corporate-tax-rates/R_code_files/"
+source_data<-"C:/Github/worldwide-corporate-tax-rates/source_data/"
+intermediate_outputs<-"C:/Github/worldwide-corporate-tax-rates/intermediate_outputs/"
+final_data<-"C:/Github/worldwide-corporate-tax-rates/final_data/"
+final_outputs<-"C:/Github/worldwide-corporate-tax-rates/final_outputs/"
 
 
 #Define Using function####
@@ -36,7 +36,7 @@ using<-function(...,prompt=TRUE){
     }
   }
 }
-setwd("C:/Users/acer/Documents/GitHub/worldwide-corporate-tax-rates/")
+setwd("C:/GitHub/worldwide-corporate-tax-rates/")
 #setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 using(gtools)
